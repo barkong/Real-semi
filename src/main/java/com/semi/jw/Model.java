79368c53421e14795af34ca9e9b4cc5930c598f1
junwoo <thdwnsdn98@gmail.com>
2022-11-18 19:00:54 +0900

    d

--- src/main/java/com/semi/jw/Model.java
+++ src/main/java/com/semi/jw/Model.java
@@ -9,13 +9,27 @@ import java.text.SimpleDateFormat;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
+
+
+
+
+
+
 public class Model {
 
 	public static void login(HttpServletRequest request) {
 
 		String userId = request.getParameter("id");
 		String userPw = request.getParameter("pw");
-
+		
+		String iddd = (String)request.getAttribute("iddd");
+		String pwww = (String)request.getAttribute("pwww");
+		
+		if (iddd!=null) {
+			userId=iddd;
+			userPw=pwww;
+		}
+		
 		Connection con = null;
 		PreparedStatement pstmt = null;
 		ResultSet rs = null;
@@ -24,12 +38,13 @@ public class Model {
 			String sql = "select * from semi_account where a_id=?";
 			con = DBManager.connect();
 			pstmt = con.prepareStatement(sql);
-			pstmt = DBManager.connect().prepareStatement(sql);
+			pstmt.setString(1, userId);
 			rs = pstmt.executeQuery();
-
+			
 			if (rs.next()) {
-				if (userPw.equals(rs.getString("a_pw"))) {
-					request.setAttribute("r", "濡쒓렇�씤 �꽦怨�!");
+				if (userPw.equals(rs.getString("a_password"))) {
+					request.setAttribute("r", "로그인 성공!");
+					
 
 					Bean bean = new Bean();
 					bean.setA_id(rs.getString("a_id"));
@@ -40,6 +55,8 @@ public class Model {
 					bean.setA_email(rs.getString("a_email"));
 					bean.setA_phone(rs.getString("a_phone"));
 					bean.setA_interest(rs.getString("a_interest"));
+					
+
 					request.setAttribute("account", bean);
 
 					HttpSession hs = request.getSession();
@@ -47,18 +64,18 @@ public class Model {
 					hs.setMaxInactiveInterval(60);
 
 				} else {
-					request.setAttribute("r", "鍮꾨�踰덊샇 �삤瑜�!");
+					request.setAttribute("r", "비밀번호 오류!");
 					;
 				}
 			} else {
-				request.setAttribute("r", "議댁옱�븯吏� �븡�뒗 �쉶�썝");
+				request.setAttribute("r", "존재하지 않는 회원");
 			}
 
 			if (pstmt.executeUpdate() == 1) {
 			}
 
 		} catch (Exception e) {
-			request.setAttribute("r", "�꽌踰� �삤瑜�");
+			request.setAttribute("r", "서버 오류");
 			e.printStackTrace();
 		} finally {
 			DBManager.close(con, pstmt, rs);
@@ -67,7 +84,7 @@ public class Model {
 	}
 
 	public static void account(HttpServletRequest request) {
-
+		
 		Connection con = null;
 		PreparedStatement pstmt = null;
 
@@ -76,34 +93,38 @@ public class Model {
 			con = DBManager.connect();
 			pstmt = con.prepareStatement(sql);
 			pstmt = DBManager.connect().prepareStatement(sql);
-
+			
+			
+			
 			pstmt.setString(1, request.getParameter("id"));
 			pstmt.setString(2, request.getParameter("pw"));
 			pstmt.setString(3, request.getParameter("name"));
-			SimpleDateFormat format = new SimpleDateFormat("dd-MMM-yyyy");
+			SimpleDateFormat format = new SimpleDateFormat("dd-MM-yyyy");
 			Date birth = (Date) format.parse(request.getParameter("birth"));
 			pstmt.setDate(4, birth);
 			pstmt.setString(5, request.getParameter("gender"));
 			pstmt.setString(6, request.getParameter("email"));
 			pstmt.setString(7, request.getParameter("phone"));
 			pstmt.setString(8, request.getParameter("chk"));
-			pstmt.executeUpdate();
+			
 
 			if (pstmt.executeUpdate() == 1) {
-				request.setAttribute("r", "�쉶�썝媛��엯�꽦怨�");
+				request.setAttribute("r", "회원가입성공");
 			}
 
 		} catch (Exception e) {
-			request.setAttribute("r", "db�꽌踰꾩삤瑜�");
+			request.setAttribute("r", "db서버오류");
 			e.printStackTrace();
 		} finally {
 			DBManager.close(con, pstmt, null);
 		}
 	}
-
+	
 	public static boolean loginCheck(HttpServletRequest request) {
 		HttpSession hs = request.getSession();
 		Bean a = (Bean) hs.getAttribute("accountInfo");
+		
+		
 
 		if (a == null) {
 			request.setAttribute("loginPage", "jsp/jw/login.jsp");
@@ -112,12 +133,51 @@ public class Model {
 		}
 		return false;
 	}
-
+	
 	public static void logout(HttpServletRequest request) {
+		
 
 		HttpSession hs = request.getSession();
 		hs.setAttribute("accountInfo", null);
 
+
+	}
+
+	public static void updateInfo(HttpServletRequest request) {
+		
+		Connection con = null;
+		PreparedStatement pstmt = null;
+
+		try {
+			String sql = "update semi_account set a_password=?,a_name=?,a_email=?,a_phone=?,a_interest where=?";
+			con = DBManager.connect();
+			pstmt = con.prepareStatement(sql);
+			pstmt = DBManager.connect().prepareStatement(sql);
+			
+			
+			
+			pstmt.setString(1, request.getParameter("pw"));
+			pstmt.setString(2, request.getParameter("name"));
+			pstmt.setString(3, request.getParameter("email"));
+			pstmt.setString(4, request.getParameter("phone"));
+			pstmt.setString(5, request.getParameter("interest"));
+			Bean a = (Bean) request.getSession().getAttribute("accountInfo");
+			pstmt.setString(6, a.getA_id());
+			pstmt.executeUpdate();
+			
+
+			if (pstmt.executeUpdate() == 1) {
+				request.setAttribute("r", "회원 정보 수정 성공");
+				request.setAttribute("iddd", a.getA_id());
+				request.setAttribute("pwww", request.getParameter("pw"));
+			}
+
+		} catch (Exception e) {
+			request.setAttribute("r", "서버오류");
+			e.printStackTrace();
+		} finally {
+			DBManager.close(con, pstmt, null);
+		}
 	}
 
 	public static void delId(HttpServletRequest request) {
--- src/main/webapp/jsp/jw/loginOk.jsp
+++ src/main/webapp/jsp/jw/loginOk.jsp
@@ -1,23 +1,16 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
-pageEncoding="UTF-8"%>
+    pageEncoding="UTF-8"%>
 <!DOCTYPE html>
 <html>
-  <head>
-    <meta charset="UTF-8" />
-    <title>Insert title here</title>
-  </head>
-  <body>
-    ${r }
-    <p>
-      ${sessionScope.accountInfo.a_id }(${sessionScope.accountInfo.a_name }) 님
-      안녕하세요
-    </p>
-    <p>
-      <button
-        onclick="location.href='InfoAccountC?id=${sessionScope.accountInfo.a_id}'"
-      >
-        마이페이지</button
-      ><button onclick="location.href='LoginC'">로그아웃</button>
-    </p>
-  </body>
+<head>
+<meta charset="UTF-8">
+<title>Insert title here</title>
+</head>
+<body>
+${r }	<p>			
+				${sessionScope.accountInfo.a_id }(${sessionScope.accountInfo.a_name }) 님 안녕하세요 <p>
+				<button onclick="location.href='InfoAccountC?id=${sessionScope.accountInfo.a_id}'">마이페이지</button><button onclick="location.href='LoginC'">로그아웃</button>
+				
+				
+</body>
 </html>
