1ccebe18466f91d781f0e832e7c92324b4582147
barkong <kimsouhyne@naver.com>
2022-11-29 21:08:52 +0900

    검색 결과 css

--- src/main/webapp/js/slideShow.js
+++ src/main/webapp/js/slideShow.js
@@ -6,8 +6,6 @@ const prev = document.querySelector('.prev'); //이전 버튼
 const next = document.querySelector('.next'); //다음 버튼
 const slideWidth = 300; //한개의 슬라이드 넓이
 const slideMargin = 100; //슬라이드간의 margin 값
-makeClone(); // 처음이미지와 마지막 이미지 복사 함수
-initfunction(); //슬라이드 넓이와 위치값 
 
 
 
@@ -16,25 +14,30 @@ slides.style.width = (slideWidth + slideMargin) * slideCount + 'px';
 
 
 function moveSlide(num) {
-  slides.style.left = -num * 200 + 'px';
-  currentIdx = num;
+	slides.style.left = -num * 205 + 'px';
+	currentIdx = num;
 }
 
-prev.addEventListener('click', function () {
-  /*첫 번째 슬라이드로 표시 됐을때는 
-  이전 버튼 눌러도 아무런 반응 없게 하기 위해 
-  currentIdx !==0일때만 moveSlide 함수 불러옴 */
-
-
-
-next.addEventListener('click', function () {
-
-  /*
-  if (currentIdx !== slideCount - 1) {
-    moveSlide(currentIdx + 1);
-  }*/
-  if (currentIdx !== 15) {
-    moveSlide(currentIdx + 1);
-
-  }
+prev.addEventListener('click', function() {
+	/*첫 번째 슬라이드로 표시 됐을때는 
+	이전 버튼 눌러도 아무런 반응 없게 하기 위해 
+	currentIdx !==0일때만 moveSlide 함수 불러옴 */
+	if (currentIdx !== 15) {
+			moveSlide(currentIdx -1);
+			}
+	
 });
+
+
+	next.addEventListener('click', function() {
+
+		/*
+		if (currentIdx !== slideCount - 1) {
+		  moveSlide(currentIdx + 1);
+		}*/
+		if (currentIdx !== 15) {
+			moveSlide(currentIdx + 1);
+
+		}
+		
+	});
